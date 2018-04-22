21ed6a057f50b1e7e7e74399102c91b49a07a80e
GedMarc <ged_marc@hotmail.com>
2018-04-22 15:47:35 +0200

    Prepping for RC1 - Update Base Package Name to new Name

--- test/com/jwebmp/plugins/bluradmin/BlurAdminPageConfiguratorTest.java
+++ test/com/jwebmp/plugins/bluradmin/BlurAdminPageConfiguratorTest.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.bluradmin;
 
-import com.jwebmp.BaseTestClass;
 import com.jwebmp.Page;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +7,7 @@ import org.junit.jupiter.api.Test;
  * @author Marc Magon
  */
 public class BlurAdminPageConfiguratorTest
-		extends BaseTestClass
+
 {
 
 	public BlurAdminPageConfiguratorTest()
@@ -18,7 +17,7 @@ public class BlurAdminPageConfiguratorTest
 	@Test
 	public void testConfigure()
 	{
-		Page p = getInstance();
+		Page p = new Page();
 		p.getOptions()
 		 .setDynamicRender(false);
 		System.out.println(p.toString(true));
