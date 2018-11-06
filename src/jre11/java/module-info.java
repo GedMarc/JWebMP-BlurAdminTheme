be10ad03a25f38ae44f62ca97cf1ff268c82f967
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:13 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/jre11/java/module-info.java
@@ -0,0 +1,24 @@
+import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.plugins.bluradmin.BlurAdminPageConfigurator;
+
+module com.jwebmp.plugins.bluradmin {
+	requires com.google.guice;
+	requires com.google.guice.extensions.servlet;
+	requires com.jwebmp.guicedinjection;
+	requires com.jwebmp.guicedservlets;
+	requires com.jwebmp.logmaster;
+	requires java.logging;
+	requires com.jwebmp.core;
+	requires com.jwebmp.plugins.bootstrap;
+	requires com.jwebmp.plugins.moment;
+	requires com.jwebmp.plugins.plusastab;
+	requires com.jwebmp.plugins.jqlayout;
+	requires com.jwebmp.plugins.weblogappender;
+	requires com.jwebmp.plugins.angularslimscroll;
+	requires com.jwebmp.plugins.toastr;
+	requires java.validation;
+	exports com.jwebmp.plugins.bluradmin;
+
+	provides IPageConfigurator with BlurAdminPageConfigurator;
+
+}
